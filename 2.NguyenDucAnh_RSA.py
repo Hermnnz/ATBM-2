fb4fda8c507dd9243c55f30fb982dcf9d5f49f45
duc anh <d.anhhhh05@gmail.com>
2025-10-01 22:33:54 +0700

    update

--- 2.NguyenDucAnh_RSA.py
+++ 2.NguyenDucAnh_RSA.py
@@ -1,8 +1,15 @@
 #  p=17, q=23, e=5
 
 
-def rsa_encrypt(Plaintext, p, q, e):
-    n = p * q
+
+
+def timnghichdao(a, m):
+    for x in range(1, m):
+        if (a * x) % m == 1:
+            return x
+    return None
+
+def mahoa(Plaintext, n, e):
     cipher = []
     for char in Plaintext:
         m = ord(char)
@@ -15,7 +22,13 @@ if __name__ == "__main__":
     q = 23
     e = 5
     Plaintext = "NguyenDucAnh"
-    cipher = rsa_encrypt(Plaintext, p, q, e)
+    n = p * q
+    z = (p - 1) * (q - 1)
+    d = timnghichdao(e, z)
+    print("n =", n)
+    print("z =", z)
+    print("Khóa công khai (n, e):", (n, e))
+    print("Khóa bí mật (n, d):", (n, d))
+    cipher = mahoa(Plaintext, n, e)
     print("Sau mã hóa:", cipher)
-
-
+    
